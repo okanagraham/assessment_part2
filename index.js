24802f945725d703ece7f54e961307d5aea29a7c
Oana Graham <okanagraham@gmail.com>
2021-07-13 14:33:12 -0400

    found Paola, Get All activities

--- index.js
+++ index.js
@@ -8,9 +8,26 @@ lib.Configuration.apiToken = '4aaf3dc71669d84f5198c16d3ad9e7ff7c214ad5'
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
+}).on('error', () => {
+  console.log('Error occured while listening')
 })
 
-app.get('/v1/activities', async (req, res) => {
-  // verify if user is Paola, send email with data if so
-  res.send()
+app.get('/', async (req, res) => {
+  // Below  code shows Paola in list with ID 11765267
+  /* const input = {}
+  input.limit = 16
+
+  const persons = await lib.PersonsController.getAllPersons(input, (person) => {
+    console.log(person)
+  }) */
+
+  // get all activities for Paola
+  const input = {}
+  input.userId = 11765267
+
+  const activities = await lib.ActivitiesController.getAllActivitiesAssignedToAParticularUser(input, () => {
+
+  })
+
+  res.send(activities)
 })

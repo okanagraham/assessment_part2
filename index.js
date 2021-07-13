13577fba0a0167a172d3e8389c723b995ff68f7d
Oana Graham <okanagraham@gmail.com>
2021-07-12 21:16:05 -0400

    installed pipedream

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-/* 
-    Assessment Part2 - Okana Graham
-    July 12th 2021
-*/
 const express = require('express')
-const parser = require('body-parser')
+const app = express()
+const lib = require('pipedrive')
 
+const PORT = 1800
+
+lib.Configuration.apiToken = '4aaf3dc71669d84f5198c16d3ad9e7ff7c214ad5'
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`)
+})
+
+app.get('/', async (req, res) => {
+  const user = await lib.UsersController.getCurrentUserData()
+  res.send(user)
+})

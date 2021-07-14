9e27756ed43027c209c5da7ef83fd063e3bb8de8
Oana Graham <okanagraham@gmail.com>
2021-07-13 23:32:00 -0400

    send alert if new data

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const app = express()
 const lib = require('pipedrive')
+const nodemailer = require('nodemailer')
 
+const mailTransport = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'okanagraham@gmail.com',
+    pass: 'innymtmrmtwxhmbp'
+  }
+})
 const PORT = 1800
 
 lib.Configuration.apiToken = '4aaf3dc71669d84f5198c16d3ad9e7ff7c214ad5'
@@ -23,19 +31,31 @@ app.get('/', async (req, res) => {
   const firstRun = lib.ActivitiesController.getAllActivitiesAssignedToAParticularUser({ userId: 11765267, limitId: 11765267, done: 0 }, () => {
   })
 
-  await Promise.all([firstRun]).then((result) => { // get Paola's activities
-    console.log('Last item ID: ', result[0].data[result[0].data.length - 1].id) // get the last item
+  await Promise.all([firstRun]).then((firstResult) => { // get Paola's activities
+    console.log('Last item ID: ', firstResult[0].data[firstResult[0].data.length - 1].id) // get the last item
     setTimeout(async () => { // then sleep for allotted time
       const secondRun = await lib.ActivitiesController.getAllActivitiesAssignedToAParticularUser({ userId: 11765267, limitId: 11765267, done: 0 }, () => {
-      }) // next get the list again
-      console.log('Second Run length', secondRun.data.length)
-      // compare list then email if list different
-      if (secondRun.data[secondRun.data.length] === result[0].data[result[0].data.length]) {
+      })
+      // DEBUG: console.log('Second Run length', secondRun.data.length)
+      // get the last item ID, then compare to the last item id of the second run, if second run
+      // id is greater, then the list has changed
+      if (secondRun.data[secondRun.data.length - 1].id === firstResult[0].data[firstResult[0].data.length - 1].id) {
         // this item is the same
+        // DEBUG console.log('no change')
       } else {
-        console.log('DIfferent Item')
+        const mailOptions = {
+          from: 'Assessment2 <noreply@okanagraham.com>',
+          to: 'okanagraham@gmail.com',
+          subject: 'New Activity found for Paolo',
+          html: 'Content goes here'
+        }
+        await mailTransport.sendMail(mailOptions, function (err, info) {
+          if (err) {
+            console.log('Error found')
+          }
+        })
       }
-    }, 120000)
+    }, 10000)
   })
 
   res.sendStatus(200)
